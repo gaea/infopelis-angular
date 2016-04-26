bf7d9d8b7b9219f64a2d3df5074dd20a4b456551
gaea <gaea.uv@gmail.com>
2016-04-26 11:59:47 -0500

    added order movies functionality

--- app/js/controllers/movieController.js
+++ app/js/controllers/movieController.js
@@ -4,10 +4,15 @@
 
 var movieController = angular.module('movieController', []);
 
-movieController.controller('MovieListCtrl', ['$scope', '$routeParams', 'MovieService',
-  function($scope, $http, MovieService) {
+movieController.controller('MovieListCtrl', ['$scope', '$filter', '$routeParams', 'MovieService',
+  function($scope, $filter, $routeParams, MovieService) {
     $scope.movies = [];
     $scope.page = 1;
+    $scope.search = false;
+    $scope.date = new Date();
+    $scope.predicate = 'release_date';
+    $scope.reverse = false;
+    var orderBy = $filter('orderBy');
     
 
     $scope.loadNextPage = function() {
@@ -22,16 +27,24 @@ movieController.controller('MovieListCtrl', ['$scope', '$routeParams', 'MovieSer
       }
     }
 
+    $scope.order = function(predicate) {
+      $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+      $scope.predicate = predicate;
+
+      $scope.movies = orderBy($scope.movies, predicate, $scope.reverse);
+    };
+
     $scope.loadPage = function(page) {
       MovieService.discover(page).success(function(movies){
         console.log(movies);
-        $scope.movies = movies.results;
+        $scope.movies = $scope.movies.concat(movies.results);
       }).error(function(error){
         console.log(error.status_code + error.status_message);
       });
     }
 
     $scope.loadPage($scope.page);
+    //$scope.order('release_date');
   }
 ]);
 
