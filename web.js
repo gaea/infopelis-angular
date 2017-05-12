662ab8aa34420f92f6a4448fce6e73a78b115a11
Gustavo Espinosa <gespinosa@brangerbriz.com>
2017-05-12 13:52:23 -0500

    fixed js project path

--- web.js
+++ web.js
@@ -2,10 +2,7 @@ var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-//app.use(express.static(__dirname + "/app"));
-
-console.log("dirname: " + __dirname);
+app.use(gzippo.staticGzip("" + __dirname + "/app"));
 
 app.get('/', function(req, res){
 	res.sendfile('index.html', {root: __dirname })
